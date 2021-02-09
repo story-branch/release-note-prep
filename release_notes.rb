b9e73b1b272cce39d8680e8ffeba754909a9f970
Rui Baltazar <rui.p.baltazar@gmail.com>
2021-02-09 10:46:16 +0800

    first pass of release note

---
+++ release_notes.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'github_api'
+
+token = ENV['CHANGELOG_GITHUB_TOKEN']
+
+github = Github.new(oauth_token: token, user: 'PerxTech', repo: 'perx-api')
+prs = github.pull_requests.list(state: 'closed', sort: 'updated', direction: 'desc')
+
+last_release_index = prs.to_a.index do |pr|
+  /Prepare Release/i =~ pr[:title]
+end
+
+prs_for_release_notes = prs[0...last_release_index]
+
+puts "Prepare Release"
+
+prs_for_release_notes.each do |pr|
+  puts "##{pr[:number]} #{pr[:title]}"
+end
