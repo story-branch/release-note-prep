434fd3b0e21db0969825ff597e59b31ec12149ba
Rui Baltazar <rui.p.baltazar@gmail.com>
2021-05-07 11:49:37 +0800

    trying to update the pr description

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,6 @@ echo '::endgroup::'
 
 echo '::group:: Running script'
 RELEASE_NOTES=$(ruby /release_notes.rb)
-echo "::set-output name=changelog::$(RELEASE_NOTES)"
 
 echo "RELEASE_NOTES<<EOF" >> $GITHUB_ENV
           echo "$RELEASE_NOTES" >> $GITHUB_ENV
