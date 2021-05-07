7d54c3ba388eaf265d5986cb9e00a10ba9f4b094
Rui Baltazar <rui.p.baltazar@gmail.com>
2021-05-07 10:10:08 +0800

    trying to run ruby script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+echo '::group:: Installing github_api gem'
+gem install -N github_api
+echo '::endgroup::'
+
+export CHANGELOG_GITHUB_TOKEN="${INPUT_GITHUB_TOKEN}"
+echo '::group:: Running script'
+ruby release_notes.rb
+echo '::endgroup::'
